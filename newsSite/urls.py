2ac8e1a458604ada8e1eef40843ffb20ca44fa0f
darow <xerosyv@gmail.com>
2022-02-11 19:51:35 +0300

    static+header

--- newsSite/urls.py
+++ newsSite/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.shortcuts import redirect
+from django.urls import path, include, reverse
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', lambda request: redirect(reverse('news:news_list'), permanent=False)),
     path('news/', include('news.urls')),
     path('admin/', admin.site.urls),
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
